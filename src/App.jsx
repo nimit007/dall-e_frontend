77cda974f7e097ea3bdad5bcd960350f92d95f2e
Nimit012 <nimit.jain@comprotechnologies.com>
2023-07-29 14:37:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { logo } from "./assets";
+import {Home, CreatePost} from './pages'
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
